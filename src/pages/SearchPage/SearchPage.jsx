5fdffefbc7020f50c60fcca10801e6d36a110cc6
Sreehari P V <sreehari.valsan@calpinetech.com>
2023-11-21 13:55:56 +0530

    Added debouncing in the Search Page Component

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -7,43 +7,64 @@ import axios from "axios";
 const SearchPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [page, setPage] = useState(1);
-  const [searchData, setSearchData] = useState({ pages: 1, data: [] });
+  const [searchData, setSearchData] = useState({
+    pages: 1,
+    data: [],
+    totalPages: 0,
+  });
   const [loading, setLoading] = useState(false);
 
-  const handleFormSubmit = async (e) => {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
+    // Perform the search after a delay (e.g., 300ms) to debounce the input
     setLoading(true);
-    const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${
-      import.meta.env.VITE_API_KEY
-    }`;
-    try {
-      const response = await axios.get(
-        `${searchUrl}&query=${searchTerm}&page=${page}`
-      );
-      setSearchData({ pages: response.data.page, data: response.data.results });
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    } finally {
-      setLoading(false);
-    }
   };
+
+  useEffect(() => {
+    // Check if searchTerm is not empty before making the API call
+    if (searchTerm !== "") {
+      const debounceTimeout = setTimeout(() => {
+        const fetchData = async () => {
+          const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${
+            import.meta.env.VITE_API_KEY
+          }`;
+
+          try {
+            const response = await axios.get(
+              `${searchUrl}&query=${searchTerm}&page=${page}`
+            );
+            console.log(response.data);
+            setSearchData({
+              ...searchData,
+              pages: response.data.page,
+              data: response.data.results,
+              totalPages: response.data.total_pages,
+            });
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          } finally {
+            setLoading(false);
+          }
+        };
+
+        fetchData();
+      }, 700); // Adjust the delay as needed (e.g., 300ms)
+
+      // Cleanup the timeout on each input change
+      return () => clearTimeout(debounceTimeout);
+    }
+  }, [searchTerm, page]); // Trigger the effect on changes to searchTerm and page
 
   return (
     <div className={styles.container}>
       <form action="" onSubmit={handleFormSubmit}>
-        <div className="max-w-[500px] flex gap-5">
+        <div className="w-full flex gap-5 min-h-[50px]">
           <input
             type="text"
             placeholder="Search..."
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="min-w-[100%] pl-5 min-h-full focus:border-none focus:outline-none rounded-md"
+            className="min-w-full md:min-w-[55%] lg:min-w-[40%] pl-5 min-h-full focus:border-none focus:outline-none rounded-md"
           />
-          <button
-            className={`${styles.searchBtn} btn bg-primary lg:hover:bg-white search-btn`}
-            type="submit"
-          >
-            <FiSearch fontSize={20} className={styles.searchIcon} />
-          </button>
         </div>
       </form>
       <div className="pt-5 mt-2 flex justify-between md:justify-start md:gap-[4rem] bg-background sticky top-[30px]">
@@ -52,11 +73,13 @@ const SearchPage = () => {
           onClick={() => setPage((prevState) => prevState - 1)}
           className="btn"
         >
-          {" "}
-          Previous{" "}
+          Previous
         </button>
-        <span>{page}</span>
+        <span className="btn">
+          {page} of {searchData.totalPages}
+        </span>
         <button
+          disabled={page === searchData.totalPages ? true : false}
           className="btn"
           onClick={() => setPage((prevState) => prevState + 1)}
         >
